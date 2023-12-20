540f078b4803745ec6796bdcc3450077c4a032cf
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2023-12-20 22:54:41 +0100

    day12 question 1 done

--- src/main/java/adventofcode/adventofcode/solving/DayTwelve.java
+++ src/main/java/adventofcode/adventofcode/solving/DayTwelve.java
@@ -3,6 +3,7 @@ package adventofcode.adventofcode.solving;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DayTwelve {
@@ -29,17 +30,44 @@ public class DayTwelve {
     }
 
     private int combination(String line, int[] argument) {
+        //System.out.println("Iteration : "+line);
+        if(line.length() < 3) {
+            return 0;
+        }
+
+        //S'il n'y a plus de ? on arrete de diviser le String et on retourne 1 (il faudra implémenter ici la validation de la ligne)
         if (!line.contains("?")) {
-            System.out.println("ici ok !");
-            if (Arrays.stream(argument).sum() == line.chars().filter(ch -> ch == '#').count()) {
-                System.out.println("toujours ok  !");
+            //System.out.println("ici ok !");
+            if (validateLine(line, argument)) {
+                //System.out.println("toujours ok  !");
                 return 1;
+            } else {
+                return 0;
             }
         }
-        String before = line.substring(0, line.indexOf('?'));
-        String after = line.substring(line.indexOf('?'));
 
+        //Cas particulier la ligne se termine par ?
+        if (line.indexOf('?') == line.length() -1 ) {
+            //System.out.println("on termine par ?");
+            return combination(line.substring(0, line.length()-1)+".", argument) + combination(line.substring(0, line.length()-1)+"#", argument);
+        } else if (line.indexOf('?') == 0) {
+            //Cas particulier la ligne commence par ?
+            //System.out.println("on commence par ?");
+            return combination("."+line.substring(1), argument) + combination("#"+line.substring(1), argument);
+        }
+
+        String before = line.substring(0, line.indexOf('?'));
+        String after = line.substring(line.indexOf('?')+1);
         return combination(before+"."+after, argument) + combination(before+"#"+after, argument);
+    }
+
+    private boolean validateLine(String line, int[] argument) {
+        Matcher matcher = PATTERN.matcher(line);
+        List<Integer> listOfSharp = new ArrayList<>();
+        while (matcher.find()) {
+            listOfSharp.add(matcher.group().length());
+        }
+        return Arrays.equals(listOfSharp.stream().mapToInt(Integer::intValue).toArray(), argument);
     }
 
     private List<int[]> generateArguments(List<String> input) {
